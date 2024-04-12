d783e8ff0aa1c54095d928b30aa910ad10382b10
Juan Salvador Tafoya Vargas <juan.tafoya137@gmail.com>
2024-04-12 13:48:19 +0100

    Pushing the postEE configuration files for gen, aod and miniaod

---
+++ scripts/cfg_creator_postEE_aod.py
@@ -0,0 +1,51 @@
+import os
+
+p = "/vols/cms/jtafoyav/production/CMSSW_13_2_0/src/Configuration/GenProduction/data"
+files = os.listdir(p)
+
+crab = """
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = '{name}'
+config.General.workArea = '{name}'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'aod_postEE_cfg.py'
+config.JobType.maxMemoryMB = 2500
+# config.JobType.numCores = 8
+
+config.Data.inputDataset = '{dataset}'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+NJOBS = 100
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.inputDBS = 'phys03'
+
+config.Data.outLFNDirBase = '/store/user/tafoyava/samples/AODSIM/'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'AODSIM_2022'
+
+config.Site.storageSite = 'T2_US_UCSD'
+"""
+
+datasets = {
+    "scenarioA_mpi_4_mA_1p33_ctau_10": "/scenarioA_mpi_4_mA_1p33_ctau_10/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_10_2022-7f5f2da1509daceb668e678ad7f6c826/USER",
+    "scenarioA_mpi_4_mA_1p33_ctau_100": "/scenarioA_mpi_4_mA_1p33_ctau_100/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_100_2022-0d52a2f512a8dff64215063478ed58d4/USER",
+    "scenarioA_mpi_4_mA_1p33_ctau_0p1": "/scenarioA_mpi_4_mA_1p33_ctau_0p1/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_0p1_2022-a9026f8b948d21e523e910a9f4d11f1c/USER",
+    "scenarioA_mpi_4_mA_1p33_ctau_1p0": "/scenarioA_mpi_4_mA_1p33_ctau_1p0/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_1p0_2022-9250a3d6ebbbc29553c17fb39e5f0303/USER",
+}
+
+for name, dataset in datasets.items():
+    # name = f.split(".")[0]
+    # print(cmnd.format(name=name))
+    # print(name)
+    if os.path.exists(f"{name}/crab_{name}"):
+        continue
+    #os.system(cmnd.format(name=name))
+    with open("crab_submit_%s.py" % name, "w+") as f:
+        f.write(crab.format(name=name, dataset=dataset))
+    os.system("crab submit crab_submit_%s.py" % name)
+
---
+++ scripts/cfg_creator_postEE_gen.py
@@ -0,0 +1,120 @@
+import os
+
+p = "Configuration/GenProduction/data/"
+files = os.listdir(p)
+
+cmnd = """
+    cmsDriver.py Configuration/GenProduction/python/{name}_cfi.py --python_filename 2022/gen_{name}_cfg.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN --fileout file:gen_{name}.root --conditions 124X_mcRun3_2022_realistic_postEE_v3 --beamspot Realistic25ns13p6TeVEarly2022Collision --step GEN --geometry DB:Extended --era Run3 --no_exec --mc -n -1
+"""
+
+crab = """
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = '{name}'
+config.General.workArea = '2022/{name}'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = '2022/gen_{name}_cfg.py'
+
+config.Data.outputPrimaryDataset = '{name}'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 1000
+NJOBS = 100
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+
+config.Data.outLFNDirBase = '/store/user/tafoyava/samples/'
+config.Data.publication = True
+config.Data.outputDatasetTag = '{name}_2022'
+
+config.Site.storageSite = 'T2_US_UCSD'
+"""
+
+
+files = [
+  "scenarioA_mpi_10_mA_1p00_ctau_0p1.slha",
+  "scenarioA_mpi_10_mA_1p00_ctau_10.slha",
+  "scenarioA_mpi_10_mA_1p00_ctau_100.slha",
+  "scenarioA_mpi_10_mA_1p00_ctau_1p0.slha",
+  "scenarioA_mpi_10_mA_3p33_ctau_0p1.slha",
+  "scenarioA_mpi_10_mA_3p33_ctau_10.slha",
+  "scenarioA_mpi_10_mA_3p33_ctau_100.slha",
+  "scenarioA_mpi_10_mA_3p33_ctau_1p0.slha",
+  "scenarioA_mpi_1_mA_0p33_ctau_0p1.slha",
+  "scenarioA_mpi_1_mA_0p33_ctau_10.slha",
+  "scenarioA_mpi_1_mA_0p33_ctau_100.slha",
+  "scenarioA_mpi_1_mA_0p33_ctau_1p0.slha",
+  "scenarioA_mpi_2_mA_0p67_ctau_0p1.slha",
+  "scenarioA_mpi_2_mA_0p67_ctau_10.slha",
+  "scenarioA_mpi_2_mA_0p67_ctau_100.slha",
+  "scenarioA_mpi_2_mA_0p67_ctau_1p0.slha",
+  "scenarioA_mpi_4_mA_0p40_ctau_0p1.slha",
+  "scenarioA_mpi_4_mA_0p40_ctau_10.slha",
+  "scenarioA_mpi_4_mA_0p40_ctau_100.slha",
+  "scenarioA_mpi_4_mA_0p40_ctau_1p0.slha",
+  "scenarioA_mpi_4_mA_1p33_ctau_0p1.slha",
+  "scenarioA_mpi_4_mA_1p33_ctau_10.slha",
+  "scenarioA_mpi_4_mA_1p33_ctau_100.slha",
+  "scenarioA_mpi_4_mA_1p33_ctau_1p0.slha",
+  "scenarioB1_mpi_1_mA_0p33_ctau_0p1.slha",
+  "scenarioB1_mpi_1_mA_0p33_ctau_10.slha",
+  "scenarioB1_mpi_1_mA_0p33_ctau_100.slha",
+  "scenarioB1_mpi_1_mA_0p33_ctau_1p0.slha",
+  "scenarioB1_mpi_2_mA_0p40_ctau_0p1.slha",
+  "scenarioB1_mpi_2_mA_0p40_ctau_10.slha",
+  "scenarioB1_mpi_2_mA_0p40_ctau_100.slha",
+  "scenarioB1_mpi_2_mA_0p40_ctau_1p0.slha",
+  "scenarioB1_mpi_2_mA_0p67_ctau_0p1.slha",
+  "scenarioB1_mpi_2_mA_0p67_ctau_10.slha",
+  "scenarioB1_mpi_2_mA_0p67_ctau_100.slha",
+  "scenarioB1_mpi_2_mA_0p67_ctau_1p0.slha",
+  "scenarioB1_mpi_4_mA_0p80_ctau_0p1.slha",
+  "scenarioB1_mpi_4_mA_0p80_ctau_10.slha",
+  "scenarioB1_mpi_4_mA_0p80_ctau_100.slha",
+  "scenarioB1_mpi_4_mA_0p80_ctau_1p0.slha",
+  "scenarioB1_mpi_4_mA_1p33_ctau_0p1.slha",
+  "scenarioB1_mpi_4_mA_1p33_ctau_10.slha",
+  "scenarioB1_mpi_4_mA_1p33_ctau_100.slha",
+  "scenarioB1_mpi_4_mA_1p33_ctau_1p0.slha",
+  "scenarioB2_mpi_1_mA_0p60_ctau_0p1.slha",
+  "scenarioB2_mpi_1_mA_0p60_ctau_10.slha",
+  "scenarioB2_mpi_1_mA_0p60_ctau_100.slha",
+  "scenarioB2_mpi_1_mA_0p60_ctau_1p0.slha",
+  "scenarioB2_mpi_2_mA_1p10_ctau_0p1.slha",
+  "scenarioB2_mpi_2_mA_1p10_ctau_10.slha",
+  "scenarioB2_mpi_2_mA_1p10_ctau_100.slha",
+  "scenarioB2_mpi_2_mA_1p10_ctau_1p0.slha",
+  "scenarioB2_mpi_4_mA_2p10_ctau_0p1.slha",
+  "scenarioB2_mpi_4_mA_2p10_ctau_10.slha",
+  "scenarioB2_mpi_4_mA_2p10_ctau_100.slha",
+  "scenarioB2_mpi_4_mA_2p10_ctau_1p0.slha",
+  "scenarioC_mpi_10_mA_8p00_ctau_0p1.slha",
+  "scenarioC_mpi_10_mA_8p00_ctau_10.slha",
+  "scenarioC_mpi_10_mA_8p00_ctau_100.slha",
+  "scenarioC_mpi_10_mA_8p00_ctau_1p0.slha",
+  "scenarioC_mpi_2_mA_1p60_ctau_0p1.slha",
+  "scenarioC_mpi_2_mA_1p60_ctau_10.slha",
+  "scenarioC_mpi_2_mA_1p60_ctau_100.slha",
+  "scenarioC_mpi_2_mA_1p60_ctau_1p0.slha",
+  "scenarioC_mpi_4_mA_3p20_ctau_0p1.slha",
+  "scenarioC_mpi_4_mA_3p20_ctau_10.slha",
+  "scenarioC_mpi_4_mA_3p20_ctau_100.slha",
+  "scenarioC_mpi_4_mA_3p20_ctau_1p0.slha",
+]
+
+
+for f in files:
+    name = f.split(".")[0]
+    if os.path.exists(f"2022/{name}/crab_{name}"):
+        continue
+    print("Launching", name)
+    # continue
+    # print(cmnd.format(name=name))
+    # print(name)
+    os.system(cmnd.format(name=name))
+    with open("2022/crab_submit_%s.py" % name, "w+") as f:
+        f.write(crab.format(name=name))
+    os.system("crab submit 2022/crab_submit_%s.py" % name)
+
