5700a803fad425b01c6ee8687abc5e9f42d99513
Juan Tafoya <juantafoya@eduroam-174194196-1.dyndns.cern.ch>
2025-01-09 17:01:49 +0100

    Updated production configuration files and request.csv file

--- scripts/merge_cfg.py
+++ scripts/merge_cfg.py
@@ -8,12 +8,25 @@ py_file_dir = "./separate_files/python/"
 slha_file_dir = "./separate_files/slha/"
 output_file_dir = "./combined_files/"
 
-request_file_path=output_file_dir+"request.csv"
+request_file_path = output_file_dir + "request.csv"
 
 def convert_file(cfg_filename):
-    py_file_path = py_file_dir+cfg_filename+"_cfi.py"
-    slha_file_path = slha_file_dir+cfg_filename+".slha"
-    output_file_path = output_file_dir+cfg_filename+"_cfi.py"
+    # dataset name must follow naming convention (https://cms-pdmv.gitbook.io/project/mccontact/rules-for-run3-2024-dataset-names)
+    # PROCESS_[BINNING]_[FILTER]_[PARAMETERS]_TUNE_BEAME_ME-PS
+    #scenarioA_mpi2_mA_0p67_ctau_100 -> GluGluHToDarkShowers_Par-ctau-100-mA-0p67-mPi2_TuneCP5_13p6TeV_powheg-pythia8
+    new_cfg_filename = "GluGluHToDarkShowers" # base process name
+    new_cfg_filename += "-" + (cfg_filename.split("_")[0]).replace("scenario","Scenario") # scenario, capital "S"
+    new_cfg_filename += "_Par" # parameters
+    new_cfg_filename += "-ctau-" + cfg_filename.split("ctau_")[1] #+ "mm" # par. ctau
+    new_cfg_filename += "-mA-" + (cfg_filename.split("mA_")[1]).split("_")[0] #+ "GeV" # par. mA
+    new_cfg_filename += "-mpi-" + (cfg_filename.split("mpi_")[1]).split("_")[0] #+ "GeV" # par. mPi
+
+    print(" Initial filename: " + cfg_filename)
+    print(" New filename: " + new_cfg_filename)
+    
+    py_file_path = py_file_dir + cfg_filename + "_cfi.py"
+    slha_file_path = slha_file_dir + cfg_filename + ".slha"
+    output_file_path = output_file_dir + new_cfg_filename + "_cfi.py"
 
     # Read the .py file
     with open(py_file_path, "r") as py_file:
@@ -66,6 +79,9 @@ def convert_file(cfg_filename):
         if "processParameters = cms.vstring(params)" in line:
             updated_py_lines.append("        processParameters = cms.vstring(\n")
             updated_py_lines.extend(formatted_slha_content)
+            updated_py_lines.append("    \"\",\n")
+            updated_py_lines.append("    \"! Tau limits to override pythia8CommonSettings configuration\",\n")
+            updated_py_lines.append("    \"ParticleDecays:limitTau0 = off\",\n")
             updated_py_lines.append("        ),\n")
         elif "Pythia8GeneratorFilter" in line:
             updated_py_lines.append(line.replace("Pythia8GeneratorFilter", "Pythia8ConcurrentGeneratorFilter"))
@@ -80,8 +96,8 @@ def convert_file(cfg_filename):
 
     # Pass configuration information to the request.csv file
     with open(request_file_path, "a") as request_file:
-        request_file.write("dqcd_"+cfg_filename+"_cfi_2024test_TuneCP5_13p6TeV_powheg-pythia8,genFragments/dqcd/"+cfg_filename+"_cfi.py,1000000,powheg,/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/powheg/V2/gg_H_quark-mass-effects_mwindow1d0_slc7_amd64_gcc10_CMSSW_12_4_8.tgz,70.62865,100.00,1.0,1.0\n")
-
+        #request_file.write("dqcd_"+cfg_filename+"_cfi_2024test_TuneCP5_13p6TeV_powheg-pythia8,genFragments/dqcd/"+cfg_filename+"_cfi.py,1000000,powheg,/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/powheg/V2/gg_H_quark-mass-effects_mwindow1d0_slc7_amd64_gcc10_CMSSW_12_4_8.tgz,70.62865,100.00,1.0,1.0\n")
+        request_file.write(new_cfg_filename + "_TuneCP5_13p6TeV_powheg-pythia8,genFragments/Hadronizer/13p6TeV/DQCD_Run3/" + new_cfg_filename + "_cfi.py,500000,powheg,/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/powheg/V2/gg_H_quark-mass-effects_mwindow1d0_slc7_amd64_gcc10_CMSSW_12_4_8.tgz,70.62865,100.00,1.0,1.0\n")
 
 
 # Overwrite the request.csv file (if it already exists) with the header of the file
