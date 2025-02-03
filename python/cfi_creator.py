6d7fe3edfd415e22620e28df6bebb7f2d944e582
Jaime Leon Holgado <jleonhol@gmail.com>
2025-02-03 10:59:20 +0000

    New HV samples

--- python/cfi_creator.py
+++ python/cfi_creator.py
@@ -6,5 +6,5 @@ files = os.listdir(p)
 for f in files:
     name = f.split(".")[0]
     print(name)
-    os.system("cp scenarioA_mpi_4_mA_1p33_ctau_10_cfi.py %s_cfi.py" % name)
-    os.system("sed -i 's/scenarioA_mpi_4_mA_1p33_ctau_10/%s/g' %s_cfi.py" % (name, name))
+    os.system("cp hiddenValleyGridPack_vector_m_10_ctau_10_xiO_1_xiL_1_cfi.py %s_cfi.py" % name)
+    os.system("sed -i 's/hiddenValleyGridPack_vector_m_10_ctau_10_xiO_1_xiL_1/%s/g' %s_cfi.py" % (name, name))
